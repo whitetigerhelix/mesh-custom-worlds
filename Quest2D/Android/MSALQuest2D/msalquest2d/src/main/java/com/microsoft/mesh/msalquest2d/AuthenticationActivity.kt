208d950a9d9c9009f88edae419b8e39d74281d32
Chris Oje <chroje@microsoft.com>
2024-05-08 17:30:00 -0700

    MSALQuest2D library project
    We can remove this part from the repo and leave just the non-library part

---
+++ Quest2D/Android/MSALQuest2D/msalquest2d/src/main/java/com/microsoft/mesh/msalquest2d/AuthenticationActivity.kt
@@ -0,0 +1,34 @@
+package com.microsoft.mesh.msalquest2d
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.identity.client.exception.MsalException
+import com.microsoft.identity.client.PublicClientApplication
+import com.microsoft.identity.client.IPublicClientApplication
+import com.microsoft.identity.client.ISingleAccountPublicClientApplication
+import com.microsoft.mesh.msalquest2d.R
+
+class AuthenticationActivity : AppCompatActivity() {
+
+	private lateinit var msalApp: ISingleAccountPublicClientApplication
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_authentication)
+
+		// Initialize MSAL using the single account public client application
+		PublicClientApplication.createSingleAccountPublicClientApplication(
+			applicationContext,
+			R.raw.msal_config,
+			object : IPublicClientApplication.ISingleAccountApplicationCreatedListener {
+				override fun onCreated(application: ISingleAccountPublicClientApplication) {
+					msalApp = application
+				}
+
+				override fun onError(exception: MsalException) {
+					// Handle the error
+				}
+			}
+		)
+	}
+}
